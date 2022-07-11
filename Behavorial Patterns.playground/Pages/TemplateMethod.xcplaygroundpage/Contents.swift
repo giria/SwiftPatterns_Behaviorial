43a6995b5c489874c07c7dcfce857bd4a26abe29
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-07-11 10:54:39 +0100

    initial commit

---
+++ Behavorial Patterns.playground/Pages/TemplateMethod.xcplaygroundpage/Contents.swift
@@ -0,0 +1,74 @@
+//: [Previous](@previous)
+/*:
+ 
+ ## Template Method
+   Allows specific steps of an algorith to be replaced by the client without modifying the structure of the algorithm.
+ 
+ ## What problems can the Template Method design pattern solve?
+ 
+ The template method is a method in a superclass, usually an abstract superclass, and defines the skeleton of an operation in terms of a number of high-level steps. These steps are themselves implemented by additional helper methods in the same class as the template method.
+
+ The helper methods may be either abstract methods, in which case subclasses are required to provide concrete implementations, or hook methods, which have empty bodies in the superclass. Subclasses can (but are not required to) customize the operation by overriding the hook methods. The intent of the template method is to define the overall structure of the operation, while allowing subclasses to refine, or redefine, certain steps.
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
+typealias File     = String
+typealias RawData  = String
+typealias `Data`   = String
+typealias Analysis = String
+/// Blueprint of the functions that will require custom implementations
+protocol DataMiner {
+    func openFile(path: String) -> File
+    func extractData(file: File) -> RawData
+    func parseData(data: RawData) -> `Data`
+    func closeFile(file: File)
+}
+extension DataMiner {
+    /// TEMPLATE METHOD: series of functions defining the algorithm
+    func mine(path: String) {
+        let file = openFile(path: path)
+        let rawData = extractData(file: file)
+        let data = parseData(data: rawData)
+        let analysis = analyzeData(analysis: data)
+        sendReport(analysis: analysis)
+        closeFile(file: file)
+    }
+    // MARK: Default implementations
+    func analyzeData(analysis: `Data`) -> Analysis {
+        print("ℹ️ analyze data")
+        return "analysis"
+    }
+    func sendReport(analysis: Analysis) {
+        print("ℹ️ send report")
+    }
+}
+// PDFDataMiner conforms to the DataMiner protocol
+class PDFDataMiner: DataMiner {
+    init() {
+        // Call the template method
+        mine(path: "PDFFilePath")
+    }
+    func openFile(path: String) -> File {
+        print("📃️ open PDF File")
+        return "PDF file opened"
+    }
+    func extractData(file: File) -> RawData {
+        print("📃️ extract PDF data")
+        return "PDF raw data extracted"
+    }
+    func parseData(data: RawData) -> `Data` {
+        print("📃️ parse PDF data")
+        return "PDF data parsed"
+    }
+    func closeFile(file: File) {
+        print("📃️ close PDF File")
+    }
+}
+// Testing the PDF implementation
+_ = PDFDataMiner()
+//: [Next](@next)
---
+++ Behavorial Patterns.playground/Pages/Visitor.xcplaygroundpage/Contents.swift
@@ -0,0 +1,90 @@
+//: [Previous](@previous)
+
+/*:
+ # Visitor Pattern
+ 
+ ## What problems can the Visitor design pattern solve?
+ 
+ - It should be possible to define a new operation for (some) classes of an object structure without changing the classes.
+ 
+ ## What solution does the Visitor design pattern describe?
+ 
+ - Define a separate (visitor) object that implements an operation to be performed on elements of an object structure.
+ - Clients traverse the object structure and call a dispatching operation accept (visitor) on an element — that "dispatches" (delegates) the request to the "accepted visitor object". The visitor object then performs the operation on the element ("visits the element").
+ This makes it possible to create new operations independently from the classes of an object structure by adding new visitor objects.
+
+ See also the UML class and sequence diagram below.
+ 
+![UML Icon](visitor.png)
+ 
+ In the UML class diagram above, the `ElementA` class doesn't implement a new operation directly. Instead, ElementA implements a dispatching operation accept(visitor) that "dispatches" (delegates) a request to the "accepted visitor object" (visitor.visitElementA(this)). The Visitor1 class implements the operation (visitElementA(e:ElementA)).
+ ElementB then implements accept(visitor) by dispatching to visitor.visitElementB(this). The Visitor1 class implements the operation (visitElementB(e:ElementB)).
+
+ The UML sequence diagram shows the run-time interactions: The Client object traverses the elements of an object structure (ElementA,ElementB) and calls accept(visitor) on each element.
+ First, the Client calls accept(visitor) on ElementA, which calls visitElementA(this) on the accepted visitor object. The element itself (this) is passed to the visitor so that it can "visit" ElementA (call operationA()).
+ Thereafter, the Client calls accept(visitor) on ElementB, which calls visitElementB(this) on the visitor that "visits" ElementB (calls operationB()).
+ 
+ 
+*/
+import Foundation
+
+protocol CommonAttributes {
+    var price: Float { get }
+}
+
+struct Book {
+    var isbn: String
+    var title: String
+    var itemPrice: Float
+}
+
+extension Book: CommonAttributes {
+    var price: Float {
+        return itemPrice
+    }
+}
+
+struct Computer {
+    var serialNumber: String
+    var brand: String
+    var unitPrice: Float
+}
+
+extension Computer: CommonAttributes {
+    var price: Float {
+        return unitPrice
+    }
+}
+
+struct Car {
+    var ean: String
+    var make: String
+    var model: String
+    var stickerPrice: Float
+}
+
+extension Car: CommonAttributes {
+    var price: Float {
+        return stickerPrice
+    }
+}
+
+let book = Book(isbn: "178-4-19-155607-0", title: "Swift Design Patterns", itemPrice: 30)
+let macBook = Computer(serialNumber: "12345-00", brand: "Apple MacBook Pro", unitPrice: 2500)
+let teslaS = Car(ean: "1KL4KXBG0AF148193", make: "Tesla", model: "S", stickerPrice: 69200)
+
+var shoppingCart = [CommonAttributes]()
+shoppingCart.append(contentsOf: [book, macBook, teslaS] as [CommonAttributes])
+
+
+func calculateTotalPrice(items: [CommonAttributes]) -> Float {
+    var price: Float = 0
+    for item in items {
+        price += item.price
+    }
+    return price
+}
+
+print(calculateTotalPrice(items: shoppingCart))
+//: [Next](@next)
+
