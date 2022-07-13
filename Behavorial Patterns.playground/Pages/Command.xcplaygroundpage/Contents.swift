10619db0b8fe71687d54a35a6d0a757d4f672db0
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-07-13 19:17:11 +0100

    command

--- Behavorial Patterns.playground/Pages/Command.xcplaygroundpage/Contents.swift
+++ Behavorial Patterns.playground/Pages/Command.xcplaygroundpage/Contents.swift
@@ -17,9 +17,88 @@
 
  The UML sequence diagram shows the run-time interactions: The Invoker object calls execute() on a Command1 object. Command1 calls action1() on a Receiver1 object, which performs the request.
  
+ ![UML Icon](command_2.png)
+ 
+ 
  */
-import Foundation
+import UIKit
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
+protocol Command {
+    func execute()
+}
+
+struct FadeOutCommand: Command {
+    fileprivate let receiver: UIView
+    fileprivate let duration: TimeInterval
+    
+    init(receiver: UIView, duration: TimeInterval = 2.3) {
+        self.receiver = receiver
+        self.duration = duration
+    }
+    
+    func execute() {
+        UIView.animate(withDuration: duration) {
+            self.receiver.alpha = 0
+        }
+    }
+}
+
+struct FadeInCommand: Command {
+    fileprivate let receiver: UIView
+    fileprivate let duration: TimeInterval
+    
+    init(receiver: UIView, duration: TimeInterval = 2.3) {
+        self.receiver = receiver
+        self.duration = duration
+    }
+    
+    func execute() {
+        UIView.animate(withDuration: duration) {
+            self.receiver.alpha = 1
+        }
+    }
+}
+
+class AnimationController {
+    fileprivate var command: Command?
+    
+    func setCommand(_ command: Command) {
+        self.command = command
+    }
+    
+    func animate() {
+        command?.execute()
+    }
+}
+
+let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+PlaygroundPage.current.liveView = containerView
+
+let view = UIView(frame: CGRect(x: containerView.frame.width / 2, y: containerView.frame.height / 2, width: 100, height: 100))
+view.center = containerView.center
+view.backgroundColor = .green
+
+containerView.addSubview(view)
+
+let invoker = AnimationController()
+
+let fadeOutCommand = FadeOutCommand(receiver: view, duration: 2)
+
+invoker.setCommand(fadeOutCommand)
+
+let delay = 2
+DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+    
+    invoker.animate()
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay), execute: {
+        let fadeInCommand = FadeInCommand(receiver: view, duration: 2)
+        invoker.setCommand(fadeInCommand)
+        
+        invoker.animate()
+    })
+}
+
 
 //: [Next](@next)
