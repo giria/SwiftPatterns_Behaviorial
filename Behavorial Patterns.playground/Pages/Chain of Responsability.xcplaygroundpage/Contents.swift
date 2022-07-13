85427971b13fbe0104d27eb0b6fec9142336bb7d
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-07-13 21:25:59 +0100

    chain

---
+++ Behavorial Patterns.playground/Pages/Chain of Responsability.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+
+/*:
+ 
+ # Chain of responsbility
+ 
+ Organizers potential request processors in a sequence and decouples responders from callers.
+ 
+ ## What problems can the Chain of responsability  design pattern solve?
+ 
+ */
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
