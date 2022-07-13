5a68cdff612772ac2cc20aa3f743d267fd53cefc
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-07-13 12:07:01 +0100

    graphic

--- Behavorial Patterns.playground/Pages/Iterator.xcplaygroundpage/Contents.swift
+++ Behavorial Patterns.playground/Pages/Iterator.xcplaygroundpage/Contents.swift
@@ -15,9 +15,49 @@
  In the above UML class diagram, the Client class refers (1) to the Aggregate interface for creating an Iterator object (createIterator()) and (2) to the Iterator interface for traversing an Aggregate object (next(),hasNext()). The Iterator1 class implements the Iterator interface by accessing the Aggregate1 class.
 
  The UML sequence diagram shows the run-time interactions: The Client object calls createIterator() on an Aggregate1 object, which creates an Iterator1 object and returns it to the Client. The Client uses then Iterator1 to traverse the elements of the Aggregate1 object.
+ 
+ 
+ ![UML Icon](IteratorBehind.png)
+ 
  */
 import Foundation
 
-var greeting = "Hello, playground"
+struct Stack<T> {
+    fileprivate var items = [T]()
+    private var currentIndex: Int = 0
+    
+    mutating func push(_ item: T) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> T? {
+        return items.popLast()
+    }
+}
+
+extension Stack: Sequence, IteratorProtocol {
+    mutating func next() -> T? {
+        guard currentIndex < items.count else {
+            return nil
+        }
+        
+        let item = items[currentIndex]
+        currentIndex += 1
+        
+        return item
+    }
+}
+
+// Testing
+
+var stack = Stack<Int>()
+
+stack.push(1)
+stack.push(3)
+stack.push(7)
+
+for n in stack {
+    print(n)
+}
 
 //: [Next](@next)
