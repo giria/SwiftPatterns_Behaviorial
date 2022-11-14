3d6024c46a0aa855dcdf77f10af591793b042d34
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-11-14 15:21:14 +0000

    cocoaTouch

--- Behavorial Patterns.playground/Pages/Chain of Responsability.xcplaygroundpage/Contents.swift
+++ Behavorial Patterns.playground/Pages/Chain of Responsability.xcplaygroundpage/Contents.swift
@@ -17,6 +17,11 @@
  In the above UML class diagram, the Sender class doesn't refer to a particular receiver class directly. Instead, Sender refers to the Handler interface for handling a request (handler.handleRequest()), which makes the Sender independent of which receiver handles the request. The Receiver1, Receiver2, and Receiver3 classes implement the Handler interface by either handling or forwarding a request (depending on run-time conditions).
  The UML sequence diagram shows the run-time interactions: In this example, the Sender object calls handleRequest() on the receiver1 object (of type Handler). The receiver1 forwards the request to receiver2, which in turn forwards the request to receiver3, which handles (performs) the request.
  
+ ## Where you can find this pattern?
+ 
+ On the Cooca Touch UI Responder, iOS relies on this pattern to process user events.
+ 
+ 
  */
 import Foundation
 
